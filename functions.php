23478b2a23f5b711612ad4232e57f3eb183d27ea
Jehdi Aizon <jaizon2000@gmail.com>
2021-06-06 20:24:15 -0600

    Initialize

--- functions.php
+++ functions.php
@@ -3,11 +3,14 @@
 // load stylesheets
 function load_css()
 {
-    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', [], 5.0, 'all');
     wp_enqueue_style('bootstrap');
 
-    wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all');
+    wp_register_style('main', get_template_directory_uri() . '/css/style.css', [], 1.0, 'all');
     wp_enqueue_style('main');
+    
+    wp_register_style('style', get_template_directory_uri() . '/dist/app.css', [], 1.0, 'all');
+    wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts', 'load_css');
 
@@ -16,7 +19,7 @@ function load_js()
 {
     wp_enqueue_script('jquery');
 
-    wp_register_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, true);
+    wp_register_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', ['jquery'], 5.0, true);
     wp_enqueue_script('bootstrapjs');
 }
 
